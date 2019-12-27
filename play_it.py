eaffbc48a81c256b5e0e0b5f0c3d275c8b12f647
Paulus Kutrich <pkutrich@gmail.com>
2019-12-27 09:37:55 -0800

    Cleanup

--- play_it.py
+++ play_it.py
@@ -30,23 +30,22 @@ class GamePlayer:
         elif http == 'post':
             response = requests.post(URL + suffix, headers=header, json=data)
 
-        # If status 4xx or 5xx, raise error.
-        response.raise_for_status()
-        # Jsonify.
+        response.raise_for_status()  # If status 4xx or 5xx, raise error.
         response = response.json()
+
         # Handle response.
         if 'cooldown' in response:
             self.cooldown = int(response['cooldown'])
         if 'errors' in response:
             if response['errors']:
                 print(f'Error: {response["errors"]}')
-        # Reset timer.
-        self.then = datetime.now()
+
+        self.then = datetime.now()  # Reset timer.
         return response
 
     def get_key(self) -> None:
-        """Register a Player and get api key"""
-        form = {"username": "Paulus",
+        """Register a Player, get/set api key, set authorization dict."""
+        form = {"username": "Paulus5",
                 "password1": "testpassword",
                 "password2": "testpassword"}
         response = self.make_request(suffix='api/registration/', data=form, http='post')
@@ -70,42 +69,44 @@ class GamePlayer:
         """Return list of all exits from <room>."""
         return self.world[room]['meta']['exits']
 
-    def BFT(self) -> list:
+    def BFS_UE(self) -> list:
         """Create path to nearest unexplored exit."""
         visited = set()
         queue = deque()
         path = (self.current_room, '')
         queue.append([path])
+
         while queue:
             path = queue.popleft()
             room = path[-1][0]
             if room not in visited:
                 visited.add(room)
-                directions = self.get_exits(room)
-                if any([self.world[room][f'to_{direction}'] is None for direction in directions]):
-                    print(f'Dead-end reached. Path back to unexplored exit: {[d for _, d in path if d]}')
-                    return path[1:]
-                for direction in directions:
-                    new_room = self.world[room][f'to_{direction}']
-                    new_path = [*path, (new_room, direction)]
+                exits = self.get_exits(room)
+                # Stop if any exit is unexplored.
+                if any([self.world[room][f'to_{exit_}'] is None for exit_ in exits]):
+                    return path[1:]  # Omit the room we're already in.
+                # Add a new path to the queue for each exit.
+                for exit_ in exits:
+                    new_room = self.world[room][f'to_{exit_}']
+                    new_path = [*path, (new_room, exit_)]
                     queue.append(new_path)
 
-    def DFT(self) -> None:
+    def DFS_DE(self) -> None:
         """Take first available unexplored exit until there are no unexplored exits."""
         rev_dir = {'n': 's', 'w': 'e', 's': 'n', 'e': 'w'}
         while True:
-            directions = self.get_exits(self.current_room)
+            exits = self.get_exits(self.current_room)
 
             # If we've explored all exits, time to BFS.
-            if all([self.world[self.current_room][f'to_{direction}'] is not None for direction in directions]):
+            if all([self.world[self.current_room][f'to_{exit_}'] is not None for exit_ in exits]):
                 return
 
             # Get all unexplored exits.
-            directions = [d for d in directions if self.world[self.current_room][f'to_{d}'] is None]
+            open_exits = [d for d in exits if self.world[self.current_room][f'to_{d}'] is None]
 
             # Take the first open path.
-            direction = directions[0]
-            new_room = self.move(direction)
+            open_exit = open_exits[0]
+            new_room = self.move(open_exit)
             new_room_id = int(new_room['room_id'])
 
             # Mark new rooms and connections on our map.
@@ -115,35 +116,37 @@ class GamePlayer:
                                            'to_w': None,
                                            'to_s': None,
                                            'to_e': None}
-            self.world[self.current_room][f'to_{direction}'] = new_room_id
-            self.world[new_room_id][f'to_{rev_dir[direction]}'] = self.current_room
+            self.world[self.current_room][f'to_{open_exit}'] = new_room_id
+            self.world[new_room_id][f'to_{rev_dir[open_exit]}'] = self.current_room
 
             # Mark all non-exits.
-            for way in ['n', 'w', 's', 'e']:
-                if way not in self.get_exits(self.current_room):
-                    self.world[self.current_room][f'to_{way}'] = False
+            for exit_ in ['n', 'w', 's', 'e']:
+                if exit_ not in exits:
+                    self.world[self.current_room][f'to_{exit_}'] = False
 
             # Update our current place in the map.
             self.current_room = new_room_id
 
     def take_path(self, path: list) -> None:
-        """Move along the path created by BFS to find the nearest room with an unexplored exit."""
-        for next_ in path:
-            room = next_[0]
-            direction = next_[1]
+        """Move along the <path> created by BFS to find the nearest room with an unexplored exit."""
+        print(f'Dead-end reached. Path back to unexplored exit: {", ".join([direction for _, direction in path])}')
+        for next_room in path:
+            room = next_room[0]
+            direction = next_room[1]
             new_room = self.move(direction, room=room)
             self.current_room = int(new_room['room_id'])
 
     def traverse_map(self) -> None:
-        """Do a DFS to dead-end, BFS to unexplored exit to create world map."""
-        print('Building map...')
+        """Do a DFS to dead-end, BFS to an unexplored exit to create world map."""
+        print('\nBuilding map...')
         while True:
-            print(f'{len(self.world)} rooms found! Current cooldown: {self.cooldown}')
-            self.DFT()
-            more_to_explore = self.BFT()
+            self.DFS_DE()
+            more_to_explore = self.BFS_UE()
             if not more_to_explore:
+                print('Map complete!\n')
                 return
             self.take_path(more_to_explore)
+            print(f'{len(self.world)} rooms found!')
 
     def move(self, direction: str, room: int = None) -> dict:
         """Move player in the given <direction>."""
@@ -153,8 +156,8 @@ class GamePlayer:
             data = {"direction": f'{direction}'}
         suffix = 'api/adv/move/'
         new_room = self.make_request(suffix=suffix, header=self.auth, data=data, http='post')
-        print(f'\n{new_room["messages"][0]}')
-        print(f'In room {new_room["room_id"]}')
+        print(f'\n{" ".join(new_room["messages"])}')
+        print(f'In room {new_room["room_id"]}. \nCurrent cooldown: {self.cooldown}')
         if new_room['items'] and not self.encumbered:
             for item in new_room['items']:
                 self.take(item)
